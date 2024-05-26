af7a2d4026a69d45d559839b4a3b1c5abae06ee5
Penon Mickael <penon.mickael@orange.fr>
2024-05-26 21:05:32 +0200

    Commit Maven du 26/05/2024

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,8 @@
 package dev;
 
+import java.util.ResourceBundle;
+
+import com.github.lalyos.jfiglet.FigletFont;
 /**
  * Hello world!
  *
@@ -8,6 +11,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "New Hello World!" );
+        //System.out.println( "New Hello World!" );
+        String titre = ResourceBundle.getBundle("application").getString("titre");
+         //String asciiArt = FigletFont.convertOneLine("New hello2!");
+        String asciiArt = FigletFont.convertOneLine(titre);
+        System.out.println(asciiArt);
     }
 }
